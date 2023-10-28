b9464c7599ab24bedb9858dfc928f253f9e2ccfe
gmaslov-dev <gennadiy.dev@yandex.ru>
2023-10-28 20:37:49 +0300

    add php-cli-tools & update README.md

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+namespace BrainGames\Cli;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use function cli\line;
+use function cli\prompt;
+
+function sayHello() {
+	line("Welcome to the Brain Games!");
+	$name = prompt("May I have your name?");
+	line("Hello, %s!", $name);
+}
